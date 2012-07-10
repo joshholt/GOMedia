35fdb3d229469389276c5a33add17cfeb5334ff2
Joshua Holt <holt.josh@gmail.com>
2012-07-10 11:53:58 -0400

    More godoc strings

--- src/gomedia/gomedia.go
+++ src/gomedia/gomedia.go
@@ -10,16 +10,24 @@ import (
 	"com.mrd/types"
 )
 
+// filePrefix defines the path the SPA frontend uses to browse directories and
+// load your music files
 const (
 	filePrefix = "/f/"
 )
 
+// Command Line Flags
+// addr -> the port on which this server runs
+// root -> the optional root location of your music files
+// stdRoot -> just provide your username if you store your music in the std iTunes Location
 var (
 	addr    = flag.String("port", ":8080", "http listen address")
 	root    = flag.String("root", "/User/<username>/Music/iTunes/", "music root")
 	stdRoot = flag.String("user", "osxuser", "your username if your itunes library is in a standard location")
 )
 
+// main kicks everything off by parsing the command line flags, sets up the web
+// handlers and starts the httpserver.
 func main() {
 	flag.Parse()
 	web.Setup(*addr, *root, *stdRoot)
